779c86f6954f46f062787edea3b00391a8137a32
Carls2320 <devencarlo3@gmail.com>
2025-01-31 14:53:09 +0700

    revisi

--- src/componen/form/FormSection.jsx
+++ src/componen/form/FormSection.jsx
@@ -1,4 +1,4 @@
-import { img10 } from "../asset";
+import { img10 } from "./asset";
 
 const ContactForm = () => {
   return (
@@ -11,11 +11,11 @@ const ContactForm = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="flex flex-col lg:flex-row items-start w-full max-w-[90%] 2xl:max-w-[100%] 2xl:max-h-[100%] sm:mt-[79px] sm:mb-[79px] 2xl:mt-[-125px] 2xl:px-10">
+      <div className="flex flex-col lg:flex-row items-start w-full 2xl:max-w-[100%] 2xl:max-h-[100%] sm:mt-[79px] sm:mb-[79px] 2xl:mt-[-125px] 2xl:px-10">
         {/* Bagian Kiri */}
-        <div className="flex-1 p-5 mb-10 lg:mb-0 sm mt-20 2xl:mt-28">
+        <div className="flex-1 p-5 mb-10 lg:mb-0 md:ml-28 2xl:mt-28 md:max-w-[40%]">
           {/* Judul utama */}
-          <h2 className="text-[32px] font-[550] leading-[48px] tracking-[0.005em] text-left mb-[39px] 2xl:text-[36px] 2xl:leading-[52px]">
+          <h2 className="text-[32px] font-[550] leading-[48px] tracking-[0.005em] text-left mt-16 mb-[39px] 2xl:text-[36px] 2xl:leading-[52px]">
             Isi formulir di samping ini dan kami akan menghubungi Anda secepat
             mungkin
           </h2>
@@ -28,7 +28,12 @@ const ContactForm = () => {
         </div>
 
         {/* Bagian Form */}
-        <div className="flex-1 bg-white text-[#212121] rounded-[25px] 2xl:mt-[100px] p-[40px] border-[8px] border-[#F086A4] w-full 2xl:px-[60px] 2xl:py-[50px]">
+
+        <div
+          className="bg-white text-[#212121] mt-5  rounded-[25px] md:ml-24 md:mt-52 p-[40px] border-[8px] border-[#F086A4] md:w-[794px] 2xl:px-[60px] 2xl:py-[50px]"
+        >
+
+
           {/* Judul form */}
           <h3 className="text-[32px] font-[550] mb-4 text-[#212121] 2xl:text-[28px] 2xl:mb-6">
             Hubungi Tim Terbaik Kami
